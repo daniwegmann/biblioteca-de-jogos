87201792449078bc37f4a466e0ec68247d4c6b19
daniwegmann <124391230+daniwegmann@users.noreply.github.com>
2023-08-03 14:57:24 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Game from "./components/Game"
+import NewGameForm from "./components/NewGameForm"
+import useGameCollection from "./hooks/useGameCollection"
+
+export default function App() {
+    const { games, addGame, removeGame } = useGameCollection()
+
+    return (
+    <div className="app">
+      <h1>Biblioteca de Jogos</h1>
+      <NewGameForm addGame={addGame} />
+      <div className="games">
+        {games.map((game) => (
+          <Game 
+          key={game.id}
+          title={game.title}
+          cover={game.cover}
+          onREmove={() => removeGame(game.id)}
+          />
+        ))}
+      </div>
+    </div>
+  )
+}
+
